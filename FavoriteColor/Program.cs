4a55b6120a2b1941499e5395a20878d81150c1b6
DaniilStropolov <daniil.stropolov@rambler.ru>
2025-02-24 00:50:49 +0300

    1) Добавил цикл for; 2) Теперь после вывода любимого цвета консоль возвращается к изначальному черному цвету.

--- FavoriteColor/Program.cs
+++ FavoriteColor/Program.cs
@@ -1,34 +1,48 @@
-﻿namespace FavoriteColor
+﻿using System.Drawing;
+
+namespace FavoriteColor
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Напишите свой любимый цвет на английском языке\nс маленькой буквы:");
-            string color = Console.ReadLine();
-            switch(color)
+            for (int i = 1; i <= 5; i++)
             {
-                case "red":
-                    Console.BackgroundColor = ConsoleColor.Red;
-                    Console.ForegroundColor = ConsoleColor.Black;
-                    Console.WriteLine("Your favorite color is red!");
-                    break;
-                case "green":
-                    Console.BackgroundColor = ConsoleColor.Green;
-                    Console.ForegroundColor = ConsoleColor.Black;
-                    Console.WriteLine("Your favorite color is green!");
-                    break;
-                case "cyan":
-                    Console.BackgroundColor = ConsoleColor.Cyan;
-                    Console.ForegroundColor = ConsoleColor.Black;
-                    Console.WriteLine($"Your favorite color is {color}!");
-                    break;
-                default:
-                    Console.BackgroundColor = ConsoleColor.Yellow;
-                    Console.ForegroundColor = ConsoleColor.Black;
-                    Console.WriteLine($"Your favorite color is {color}!");
-                    break;
+                Console.WriteLine($"\nИтерация {i}");
+                switch (Console.ReadLine())
+                {
+                    case "red":
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("Your favorite color is red!");
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        Console.ForegroundColor = ConsoleColor.White;
+                        break;
+                    case "green":
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("Your favorite color is green!");
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        Console.ForegroundColor = ConsoleColor.White;
+                        break;
+                    case "cyan":
+                        Console.BackgroundColor = ConsoleColor.Cyan;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine($"Your favorite color is cyan!");
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        Console.ForegroundColor = ConsoleColor.White;
+                        break;
+                    default:
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine($"Your favorite color is yellow!");
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        Console.ForegroundColor = ConsoleColor.White;
+                        break;
+                }
             }
+            
         }
     }
 }

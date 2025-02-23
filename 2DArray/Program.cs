dcb00d49d397e2a31c2a0bab864e1a76ce3d239d
DaniilStropolov <daniil.stropolov@rambler.ru>
2025-02-24 01:36:45 +0300

    Создал программу для работы с двумерным массивом

---
+++ 2DArray/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _2DArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] numbers = {{1, 2, 3}, {4, 5, 6}};
+            Console.WriteLine("Третий элемент второй строки: " + numbers[1, 2] + "\n");
+            Console.Write("Все элементы массива: ");
+            foreach (var number in numbers)
+            {
+                Console.Write(number + " ");
+            }
+            Console.WriteLine("\n\nДлина массива: " + numbers.Length);
+        }
+    }
+}
---
+++ NameArray/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace NameArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите свое имя:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Ваше имя по буквам:");
+            foreach (char letter in name)
+            {
+                Console.Write(letter + " ");
+            }
+            Console.WriteLine($"\nПоследняя буква вашего имени: {name[name.Length - 1]}");
+            Console.WriteLine("Ваше имя в обратном порядке:");
+            for (int i = name.Length - 1; i >= 0; i--)
+            {
+                Console.Write(name[i] + " ");
+            }
+        }
+    }
+}

d658d122cbf5395d70dec8cac534d545908bcf99
DaniilStropolov <daniil.stropolov@rambler.ru>
2025-02-24 01:08:42 +0300

    Создал программу для вывода нескольких цветов через массив

---
+++ FavoriteColorArray/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Drawing;
+
+namespace FavoriteColorArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] favoriteColors = new string[3];
+            for (int i = 0; i < favoriteColors.Length; i++)
+            {
+                Console.WriteLine($"Enter your favorite color number {i + 1}:");
+                favoriteColors[i] = Console.ReadLine();
+            }
+            foreach (var color in favoriteColors)
+            {
+                switch (color)
+                {
+                    case "red":
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("Your favorite color is red!");
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        Console.ForegroundColor = ConsoleColor.White;
+                        break;
+                    case "green":
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("Your favorite color is green!");
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        Console.ForegroundColor = ConsoleColor.White;
+                        break;
+                    case "cyan":
+                        Console.BackgroundColor = ConsoleColor.Cyan;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine($"Your favorite color is cyan!");
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        Console.ForegroundColor = ConsoleColor.White;
+                        break;
+                    default:
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine($"Your favorite color is yellow!");
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        Console.ForegroundColor = ConsoleColor.White;
+                        break;
+                }
+            }
+
+
+        }
+    }
+}

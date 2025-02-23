78ab126222d282e94d5a7b51efe9fcd991e86ebd
DaniilStropolov <daniil.stropolov@rambler.ru>
2025-02-24 00:39:03 +0300

    Add project files.

---
+++ FavoriteColor/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace FavoriteColor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Напишите свой любимый цвет на английском языке\nс маленькой буквы:");
+            string color = Console.ReadLine();
+            switch(color)
+            {
+                case "red":
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.WriteLine("Your favorite color is red!");
+                    break;
+                case "green":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.WriteLine("Your favorite color is green!");
+                    break;
+                default:
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.WriteLine($"Your favorite color is {color}!");
+                    break;
+            }
+        }
+    }
+}

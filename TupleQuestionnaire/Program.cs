a934f5dcff97d462dc8a366ccf058a4be87ed489
DaniilStropolov <daniil.stropolov@rambler.ru>
2025-02-24 21:38:22 +0300

    Создал программу-анкету, используя кортеж

---
+++ TupleQuestionnaire/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace TupleQuestionnaire
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var tuple = (name : " ", age : 0);
+            Console.WriteLine("Кортеж изначально: " + tuple);
+            Console.Write("Введите ваше имя: ");
+            tuple.name = Console.ReadLine(); 
+            Console.Write("Введите ваш возраст: ");
+            tuple.age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Итоговый кортеж: " + tuple);
+        }
+    }
+}
